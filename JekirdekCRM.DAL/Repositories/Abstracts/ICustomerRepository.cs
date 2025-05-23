c6d5ba6ef2a5392bde138bdead82cc5c67479124
Gökalp Yılmaz <gokalpyilmaz2401@gmail.com>
2025-05-23 14:27:23 +0300

    Implemented generic and concrete repositories in DAL

---
+++ JekirdekCRM.DAL/Repositories/Abstracts/ICustomerRepository.cs
@@ -0,0 +1,16 @@
+﻿using JekirdekCRM.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JekirdekCRM.DAL.Repositories.Abstracts
+{
+    public interface ICustomerRepository : IRepository<Customer>
+    {
+        //Customer's Special Methods
+        List<Customer> GetCustomerByRegion(string region);
+        List<Customer> GetCustomerByFirtName(string firstName);
+    }
+}
---
+++ JekirdekCRM.DAL/Repositories/Concretes/BaseRepository.cs
@@ -0,0 +1,59 @@
+﻿using JekirdekCRM.DAL.Context;
+using JekirdekCRM.DAL.Repositories.Abstracts;
+using JekirdekCRM.ENTITIES.Enums;
+using JekirdekCRM.ENTITIES.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JekirdekCRM.DAL.Repositories.Concretes
+{
+    public class BaseRepository<T> : IRepository<T> where T : class,IEntity
+    {
+        private readonly MyContext _context;
+
+        public BaseRepository(MyContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            entity.UpdatedAt = DateTime.UtcNow;
+            entity.Status = DataStatus.Deleted;
+            _context.SaveChanges();
+        }
+
+        public void Destroy(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public T GetById(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public List<T> GetListAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public void Update(T entity)
+        {
+            entity.UpdatedAt = DateTime.UtcNow;
+            entity.Status = DataStatus.Updated;
+            _context.Update(entity);
+            _context.SaveChanges();
+        }
+    }
+}
